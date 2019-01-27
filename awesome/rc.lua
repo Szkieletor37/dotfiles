6fae58e78353c456fb7c2cd1dc0bcd722d2bb884
szkieletor-vm <szkieletor-vm@vm.com>
2019-01-27 21:41:01 +0900

    change terminal to termite in rc.lua

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -48,7 +48,7 @@ end
 beautiful.init("/home/szkieletor/.config/awesome/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "terminator"
+terminal = "termite"
 editor = os.getenv("EDITOR") or "editor"
 editor_cmd = terminal .. " - " .. editor
 
