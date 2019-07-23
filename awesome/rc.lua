23999f186780333102f4ab773e986836d3d3ea47
szkieletor-ispc <akitosano0111@gmail.com>
2019-07-23 12:35:11 +0900

    Update: change default browser to google-chrome in awesome/rc.lua

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -274,7 +274,7 @@ globalkeys = gears.table.join(
         end,
         {description = "focus previous by index", group = "client"}
     ),
-    awful.key({ modkey, 	  }, "i", function () awful.util.spawn("chromium-browser") end),
+    awful.key({ modkey, 	  }, "i", function () awful.util.spawn("google-chrome") end),
     awful.key({ modkey,           }, "w", function () mymainmenu:show() end,
               {description = "show main menu", group = "awesome"}),
 
