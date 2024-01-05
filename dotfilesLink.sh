3066696bb6236e9c532aa07acffa8476d0f70cc2
Akito Sano <akitosano0111@gmail.com>
2024-01-05 20:34:43 +0900

    Update dotfiles path to /home/szkieletor/script/dotfiles

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,11 +1,11 @@
 #! /bin/sh
-ln -sf $HOME/dotfiles/.zshrc $HOME/.zshrc
-ln -sf $HOME/dotfiles/.Xresources $HOME/.Xresources
+ln -sf $HOME/script/dotfiles/.zshrc $HOME/.zshrc
+ln -sf $HOME/script/dotfiles/.Xresources $HOME/.Xresources
 #gakka-pc remove
 #ln -sf $HOME/dotfiles/.xprofile $HOME/.xprofile
-ln -sf $HOME/dotfiles/nvim/init.vim $HOME/.config/nvim/init.vim
-ln -sf $HOME/dotfiles/.xinitrc $HOME/.xinitrc
-ln -sf $HOME/dotfiles/awesome/rc.lua $HOME/.config/awesome/rc.lua
-ln -sf $HOME/dotfiles/awesome/theme.lua $HOME/.config/awesome/theme.lua
-ln -sf $HOME/dotfiles/termite/config $HOME/.config/termite/config
+ln -sf $HOME/script/dotfiles/nvim/init.vim $HOME/.config/nvim/init.vim
+ln -sf $HOME/script/dotfiles/.xinitrc $HOME/.xinitrc
+ln -sf $HOME/script/dotfiles/awesome/rc.lua $HOME/.config/awesome/rc.lua
+ln -sf $HOME/script/dotfiles/awesome/theme.lua $HOME/.config/awesome/theme.lua
+ln -sf $HOME/script/dotfiles/termite/config $HOME/.config/termite/config
 
