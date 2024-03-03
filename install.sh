c0724b797a8f5e051bc5c0d4536aabe70ad369ef
Akito Sano <akitosano0111@gmail.com>
2024-03-03 19:41:38 +0900

    feat: implement funcs for install status

--- install.sh
+++ install.sh
@@ -1,10 +1,30 @@
 #!/usr/bin/env bash
 
+info () {
+    printf "\r  [ \033[00;34m..\033[0m ] $1\n"
+}
+
+success () {
+    printf "\r  [ \033[00;32mOK\033[0m ] $1\n"
+
+}
+
 # install build-essential
-printf "Installing build-essential...\n"
+info 'Installing build-essential...'
 sudo apt install -y build-essential
 
+success 'Installed build-essential'
+
 # install and setup zsh
-printf "Installing zsh...\n"
+info 'Installing zsh...'
 sudo apt install -y zsh
+
+info 'setup zsh...'
 sudo chsh $(whoami) -s $(which zsh)
+
+success 'Installed and setup zsh'
+
+# setup git
+info 'Setting up git...'
+#git config --global user.name
+#git config --global user.email
